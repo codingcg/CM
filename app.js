d4aeaf58b1fa2b6fc1b7dd8883ea2db36d01b9cc
Vera Christ <verachrist@Veras-Air.SCHULE.MNSPLUS>
2023-01-24 13:23:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+//const bodyParser = require('body-parser'); // No longer Required
+//const mysql = require('mysql'); // Not required -> moved to userController
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); // New
+
+// Parse application/json
+// app.use(bodyParser.json());
+app.use(express.json()); // New
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+const handlebars = exphbs.create({ extname: '.hbs', });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+
+const userRoutes = require('./server/routes/user');
+app.use('/', userRoutes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
